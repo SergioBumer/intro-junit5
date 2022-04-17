9e69d47a2bc8d1456aca2e50a0192354aad31e90
SergioBumer <sibm.95@outlook.com>
2022-04-17 08:21:06 -0500

    Add full test lifecycle.

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,16 +1,26 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
   Greeting greeting;
+
+  @BeforeAll
+  static void beforeAll() {
+    System.out.println("Before - I am only called once!");
+  }
+
   @BeforeEach
   void setUp() {
     System.out.println("In before Each");
     greeting = new Greeting();
+  }
+
+  @AfterEach
+  void tearDown() {
+    System.out.println("In AfterEach...");
   }
 
   @Test
@@ -21,5 +31,15 @@ class GreetingTest {
   @Test
   void testHelloWorld() {
     System.out.println(greeting.helloWorld("Sergio"));
+  }
+
+  @Test
+  void testHelloWorld2() {
+    System.out.println(greeting.helloWorld("Sam"));
+  }
+
+  @AfterAll
+  static void afterClass() {
+    System.out.println("After - I am only called once!");
   }
 }
